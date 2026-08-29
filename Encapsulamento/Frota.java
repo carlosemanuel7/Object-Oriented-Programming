46ea1f138a54ac320efc548438aeef23f7abf905
carlosemanuel7 <carlosemanuel7.dev@gmail.com>
2026-08-29 17:12:32 -0300

    new car

--- Encapsulamento/Frota.java
+++ Encapsulamento/Frota.java
@@ -4,12 +4,11 @@ public class Frota {
 	public static void main (String[] args) {
 		
 		Carro c1 = new Carro(50);
+		Carro c2 = new Carro(50);
 		c1.percorrerDistancia(450);
 		c1.abastecer(100);
 		c1.percorrerDistancia(600);
-		
-		
-		
+			
 	}
 }
 
